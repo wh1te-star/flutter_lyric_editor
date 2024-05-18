9dabf504b8a52956b06e43b499a70a564abdea5b
wh1te-star <wh1736r33n@protonmail.com>
2024-05-18 23:14:00 +0900

    Add the some menu items and the class for string resources.

--- lib/appbar_menu.dart
+++ lib/appbar_menu.dart
@@ -1,35 +1,39 @@
 import 'package:flutter/material.dart';
+import 'string_resource.dart';
 
 AppBar buildAppBarWithMenu(BuildContext context) {
   return AppBar(
     title: Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        TextButton(
-          onPressed: () {
-            // Handle button press
+        DropdownButton<String>(
+          hint: const Text(StringResource.applicationMenu),
+          onChanged: (String? newValue) {
+            debugPrint('Selected Item: $newValue');
           },
-          child: Text('Action 1'),
-          style: TextButton.styleFrom(
-            iconColor: Colors.white,
-            backgroundColor: Colors.green,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5),
-            ),
-          ),
+          items:
+              <String>[StringResource.applicationMenuExit].map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
         ),
-        TextButton(
-          onPressed: () {
-            // Handle button press
+        DropdownButton<String>(
+          hint: const Text(StringResource.fileMenu),
+          onChanged: (String? newValue) {
+            debugPrint('Selected Item: $newValue');
           },
-          child: Text('Action 2'),
-          style: TextButton.styleFrom(
-            iconColor: Colors.white,
-            backgroundColor: Colors.green,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5),
-            ),
-          ),
+          items: <String>[
+            StringResource.fileMenuOpenAudio,
+            StringResource.fileMenuCreateNewLyric,
+            StringResource.fileMenuOpenLyric,
+          ].map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
         ),
       ],
     ),
---
+++ lib/string_resource.dart
@@ -0,0 +1,9 @@
+class StringResource {
+  static const String applicationMenu = "Application";
+  static const String applicationMenuExit = "Exit";
+
+  static const String fileMenu = "File";
+  static const String fileMenuOpenAudio = "Open an audio file.";
+  static const String fileMenuCreateNewLyric = "Create a new lyric file.";
+  static const String fileMenuOpenLyric = "Open a lyric file.";
+}
