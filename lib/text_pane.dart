8d2f64cd8e1f1a840edee623c57f7aa465325103
wh1te-star <wh1736r33n@protonmail.com>
2024-05-25 22:16:55 +0900

    Display a screen that resembles a terminal.

--- lib/text_pane.dart
+++ lib/text_pane.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 import 'signal_structure.dart';
+import 'package:xterm/xterm.dart';
 
 class TextPane extends StatefulWidget {
   final PublishSubject<dynamic> masterSubject;
@@ -17,57 +18,17 @@ class _TextPaneState extends State<TextPane> {
   final PublishSubject<dynamic> masterSubject;
   final FocusNode focusNode;
   _TextPaneState(this.masterSubject, this.focusNode);
-  bool isPlaying = true;
-  int time = 0;
+
+  Terminal terminal = Terminal();
 
   @override
   void initState() {
     super.initState();
-    masterSubject.stream.listen((signal) {
-      if (signal is NotifyIsPlaying) {
-        isPlaying = signal.isPlaying;
-      }
-      if (signal is NotifySeekPosition) {
-        time = signal.seekPosition;
-      }
-      updateString(isPlaying, time);
-    });
-  }
+    masterSubject.stream.listen((signal) {});
 
-  String defaultText = "Text Pane";
-
-  String formatMillisec(int inMillisecFormat) {
-    int remainingMillisec = inMillisecFormat;
-
-    int hours = remainingMillisec ~/ Duration.millisecondsPerHour;
-    remainingMillisec = remainingMillisec % Duration.millisecondsPerHour;
-
-    int minutes = remainingMillisec ~/ Duration.millisecondsPerMinute;
-    remainingMillisec = remainingMillisec % Duration.millisecondsPerMinute;
-
-    int seconds = remainingMillisec ~/ Duration.millisecondsPerSecond;
-    remainingMillisec = remainingMillisec % Duration.millisecondsPerSecond;
-
-    int millisec = remainingMillisec % Duration.millisecondsPerSecond;
-
-    String formattedHours = hours.toString().padLeft(2, '0');
-    String formattedMinutes = minutes.toString().padLeft(2, '0');
-    String formattedSeconds = seconds.toString().padLeft(2, '0');
-    String formattedMillisec = millisec.toString().padLeft(3, '0');
-
-    return "$formattedHours:$formattedMinutes:$formattedSeconds.$formattedMillisec";
-  }
-
-  void updateString(bool isPlaying, int timeMillisec) {
-    String newText;
-    if (isPlaying) {
-      newText = "Playing, ${formatMillisec(timeMillisec)}";
-    } else {
-      newText = "Stopping, ${formatMillisec(timeMillisec)}";
-    }
-    setState(() {
-      defaultText = newText;
-    });
+    terminal.onOutput = (output) {
+      debugPrint('output: $output');
+    };
   }
 
   @override
@@ -78,10 +39,7 @@ class _TextPaneState extends State<TextPane> {
         focusNode.requestFocus();
         debugPrint("The text pane is focused");
       },
-      child: Container(
-        color: Colors.blue,
-        child: Center(child: Text(defaultText)),
-      ),
+      child: TerminalView(terminal),
     );
   }
 }
