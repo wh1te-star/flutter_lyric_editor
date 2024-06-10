a80ed6862e76c2830cb4bffef9fc5f44705ac0c6
wh1te-star <wh1736r33n@protonmail.com>
2024-06-10 21:22:19 +0900

    Add the 3-stage mark length.

--- lib/scale_mark.dart
+++ lib/scale_mark.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 class ScaleMark extends CustomPainter {
   final double interval;
+  final double majorMarkLength;
+  final double midiumMarkLength;
+  final double minorMarkLength;
 
-  ScaleMark({required this.interval});
+  ScaleMark(
+      {required this.interval,
+      required this.majorMarkLength,
+      required this.midiumMarkLength,
+      required this.minorMarkLength});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -16,12 +23,27 @@ class ScaleMark extends CustomPainter {
       Offset(size.width, size.height),
       paint,
     );
-    for (double x = 0; x <= size.width; x += interval) {
-      canvas.drawLine(
-        Offset(x, size.height - 10),
-        Offset(x, size.height),
-        paint,
-      );
+    double x = 0.0;
+    for (int i = 0; x <= size.width; i++, x += interval) {
+      if (i % 10 == 0) {
+        canvas.drawLine(
+          Offset(x, size.height - majorMarkLength),
+          Offset(x, size.height),
+          paint,
+        );
+      } else if (i % 5 == 0) {
+        canvas.drawLine(
+          Offset(x, size.height - midiumMarkLength),
+          Offset(x, size.height),
+          paint,
+        );
+      } else {
+        canvas.drawLine(
+          Offset(x, size.height - minorMarkLength),
+          Offset(x, size.height),
+          paint,
+        );
+      }
     }
   }
 
