61993f0e90c28e800fbe95057f08dd106496abd9
wh1te-star <sleeeping99@outlook.com>
2024-07-14 19:43:16 +0900

    Separate the partial text painter to another file.

---
+++ lib/painter/partial_text_painter.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+
+class PartialTextPainter extends CustomPainter {
+  final String text;
+  final int start;
+  final int end;
+  final double percent;
+  final String fontFamily;
+  final double fontSize;
+  final Color fontBaseColor;
+  final double firstOutlineWidth;
+  final double secondOutlineWidth;
+  late final TextStyle textStyleBeforeInner;
+  late final TextStyle textStyleBeforeMiddle;
+  late final TextStyle textStyleBeforeOuter;
+  late final TextSpan textSpanBeforeInner;
+  late final TextSpan textSpanBeforeMiddle;
+  late final TextSpan textSpanBeforeOuter;
+  late final TextPainter textPainterBeforeInner;
+  late final TextPainter textPainterBeforeMiddle;
+  late final TextPainter textPainterBeforeOuter;
+  late final TextStyle textStyleAfterInner;
+  late final TextStyle textStyleAfterMiddle;
+  late final TextStyle textStyleAfterOuter;
+  late final TextSpan textSpanAfterInner;
+  late final TextSpan textSpanAfterMiddle;
+  late final TextSpan textSpanAfterOuter;
+  late final TextPainter textPainterAfterInner;
+  late final TextPainter textPainterAfterMiddle;
+  late final TextPainter textPainterAfterOuter;
+
+  PartialTextPainter({
+    required this.text,
+    required this.start,
+    required this.end,
+    required this.percent,
+    required this.fontFamily,
+    required this.fontSize,
+    required this.fontBaseColor,
+    required this.firstOutlineWidth,
+    required this.secondOutlineWidth,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final backgroundPaint = Paint()..color = Color(0xFFEEEEEE);
+    canvas.drawRect(
+        Rect.fromLTWH(0, 0, size.width, size.height), backgroundPaint);
+
+    setupTextStyle();
+    setupTextSpan();
+    setupTextPainter(size);
+    paintText(canvas, size);
+  }
+
+  void setupTextStyle() {
+    Shadow shadow = Shadow(
+      color: fontBaseColor,
+      blurRadius: 30.0,
+      offset: Offset(0.0, 0.0),
+    );
+    textStyleBeforeInner = TextStyle(
+      fontFamily: fontFamily,
+      fontSize: fontSize,
+      color: Colors.white,
+    );
+    textStyleBeforeMiddle = TextStyle(
+      fontFamily: fontFamily,
+      fontSize: fontSize,
+      foreground: Paint()
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = firstOutlineWidth
+        ..color = Colors.black,
+    );
+    textStyleBeforeOuter = TextStyle(
+      fontFamily: fontFamily,
+      fontSize: fontSize,
+      foreground: Paint()
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = firstOutlineWidth + secondOutlineWidth
+        ..color = fontBaseColor,
+      shadows: [shadow],
+    );
+
+    textStyleAfterInner = TextStyle(
+      fontFamily: fontFamily,
+      fontSize: fontSize,
+      color: fontBaseColor,
+    );
+    textStyleAfterMiddle = TextStyle(
+      fontFamily: fontFamily,
+      fontSize: fontSize,
+      foreground: Paint()
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = firstOutlineWidth
+        ..color = Colors.white,
+    );
+    textStyleAfterOuter = TextStyle(
+      fontFamily: fontFamily,
+      fontSize: fontSize,
+      foreground: Paint()
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = firstOutlineWidth + secondOutlineWidth
+        ..color = Colors.black,
+      shadows: [shadow],
+    );
+  }
+
+  void setupTextSpan() {
+    textSpanBeforeInner = TextSpan(text: text, style: textStyleBeforeInner);
+    textSpanBeforeMiddle = TextSpan(text: text, style: textStyleBeforeMiddle);
+    textSpanBeforeOuter = TextSpan(text: text, style: textStyleBeforeOuter);
+    textSpanAfterInner = TextSpan(text: text, style: textStyleAfterInner);
+    textSpanAfterMiddle = TextSpan(text: text, style: textStyleAfterMiddle);
+    textSpanAfterOuter = TextSpan(text: text, style: textStyleAfterOuter);
+  }
+
+  void setupTextPainter(Size size) {
+    textPainterAfterInner = TextPainter(
+        text: textSpanAfterInner,
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr);
+    textPainterAfterMiddle = TextPainter(
+        text: textSpanAfterMiddle,
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr);
+    textPainterAfterOuter = TextPainter(
+        text: textSpanAfterOuter,
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr);
+
+    textPainterAfterInner.layout(maxWidth: size.width);
+    textPainterAfterMiddle.layout(maxWidth: size.width);
+    textPainterAfterOuter.layout(maxWidth: size.width);
+
+    textPainterBeforeInner = TextPainter(
+        text: textSpanBeforeInner,
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr);
+
+    textPainterBeforeMiddle = TextPainter(
+        text: textSpanBeforeMiddle,
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr);
+
+    textPainterBeforeOuter = TextPainter(
+        text: textSpanBeforeOuter,
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr);
+
+    textPainterBeforeInner.layout(maxWidth: size.width);
+    textPainterBeforeMiddle.layout(maxWidth: size.width);
+    textPainterBeforeOuter.layout(maxWidth: size.width);
+  }
+
+  void paintText(Canvas canvas, Size size) {
+    final textWidth = textPainterBeforeInner.width;
+    final textHeight = textPainterBeforeInner.height;
+
+    final actualX = (size.width - textWidth) / 2;
+    final actualY = (size.height - textHeight) / 2;
+    final centerOffset = Offset(actualX, actualY);
+
+    canvas.clipRect(Rect.fromLTWH(0, 0, size.width, size.height));
+
+    textPainterBeforeOuter.paint(canvas, centerOffset);
+    textPainterBeforeMiddle.paint(canvas, centerOffset);
+    textPainterBeforeInner.paint(canvas, centerOffset);
+
+    final startOffset = textPainterAfterInner
+        .getOffsetForCaret(TextPosition(offset: start), Rect.zero)
+        .dx;
+    final endOffset = textPainterAfterInner
+        .getOffsetForCaret(TextPosition(offset: end), Rect.zero)
+        .dx;
+    final sliceWidth =
+        actualX + startOffset + (endOffset - startOffset) * percent;
+
+    canvas.clipRect(Rect.fromLTWH(0, 0, sliceWidth, size.height));
+
+    textPainterAfterOuter.paint(canvas, centerOffset);
+    textPainterAfterMiddle.paint(canvas, centerOffset);
+    textPainterAfterInner.paint(canvas, centerOffset);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
