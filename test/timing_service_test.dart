58b2f00b2159ef142ef576ad095ec13ab0dd5e01
wh1te-star <sleeeping99@outlook.com>
2024-08-12 15:51:15 +0900

    Write test cases of addTimingPoint function.

--- test/timing_service_test.dart
+++ test/timing_service_test.dart
@@ -7,6 +7,10 @@ import 'package:rxdart/rxdart.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
+Matcher throwsExceptionWithMessageContaining(String substring) {
+  return throwsA(predicate((e) => e is TimingPointException && e.message.contains(substring)));
+}
+
 void main() {
   group('TimingService', () {
     TestWidgetsFlutterBinding.ensureInitialized();
@@ -48,7 +52,7 @@ void main() {
       timingService = TimingService(masterSubject: masterSubject, context: mockContext);
     });
 
-    test('Test the normal inserts 1', () {
+    test('Test to add a timing point to a snippet No.1', () {
       final LyricSnippet targetSnippet = dataSetSnippet1.copyWith();
       final int characterPosition = 4;
       final int seekPosition = 2350;
@@ -66,7 +70,7 @@ void main() {
       expect(targetSnippet.timingPoints, expectedTimingPoints);
     });
 
-    test('Test the normal inserts 2', () {
+    test('Test to add a timing point to a snippet No.2', () {
       final LyricSnippet targetSnippet = dataSetSnippet1.copyWith();
       final characterPosition = 8;
       final seekPosition = 2450;
@@ -83,12 +87,133 @@ void main() {
 
       expect(targetSnippet.timingPoints, expectedTimingPoints);
     });
-    test('addTimingPoint should throw TimingPointException when index != seekIndex', () {
+
+    test('Test to throw TimingPointException when index != seekIndex No.1', () {
       final LyricSnippet targetSnippet = dataSetSnippet1.copyWith();
       final characterPosition = 8;
       final seekPosition = 2300;
 
-      expect(() => timingService.addTimingPoint(targetSnippet, characterPosition, seekPosition), throwsA(isA<TimingPointException>()));
+      expect(() => timingService.addTimingPoint(targetSnippet, characterPosition, seekPosition), throwsExceptionWithMessageContaining("contradiction"));
+    });
+
+    test('Test to add a timing point twice. No.1', () {
+      final LyricSnippet targetSnippet = dataSetSnippet1.copyWith();
+      final characterPosition = 5;
+      final seekPosition = 2450;
+      final List<TimingPoint> expectedTimingPoints = [
+        TimingPoint(3, 300),
+        TimingPoint(2, 100),
+        TimingPoint(0, 50),
+        TimingPoint(6, 650),
+        TimingPoint(4, 500),
+        TimingPoint(5, 600),
+      ];
+
+      timingService.addTimingPoint(targetSnippet, characterPosition, seekPosition);
+
+      expect(targetSnippet.timingPoints, expectedTimingPoints);
+    });
+
+    test('Test to add a timing point twice. No.2', () {
+      final LyricSnippet targetSnippet = dataSetSnippet1.copyWith();
+      final characterPosition = 5;
+      final seekPosition = 2350;
+      final List<TimingPoint> expectedTimingPoints = [
+        TimingPoint(3, 300),
+        TimingPoint(2, 50),
+        TimingPoint(0, 50),
+        TimingPoint(6, 700),
+        TimingPoint(4, 500),
+        TimingPoint(5, 600),
+      ];
+
+      timingService.addTimingPoint(targetSnippet, characterPosition, seekPosition);
+
+      expect(targetSnippet.timingPoints, expectedTimingPoints);
+    });
+
+    test('Test to add a timing point to a snippet that have a char position with 2 timing point. No.1', () {
+      final LyricSnippet targetSnippet = dataSetSnippet2.copyWith();
+      final characterPosition = 4;
+      final seekPosition = 5450;
+      final List<TimingPoint> expectedTimingPoints = [
+        TimingPoint(3, 400),
+        TimingPoint(1, 50),
+        TimingPoint(1, 250),
+        TimingPoint(7, 700),
+        TimingPoint(0, 100),
+        TimingPoint(5, 600),
+        TimingPoint(2, 200),
+      ];
+
+      timingService.addTimingPoint(targetSnippet, characterPosition, seekPosition);
+
+      expect(targetSnippet.timingPoints, expectedTimingPoints);
+    });
+
+    test('Test to add a timing point to a snippet that have a char position with 2 timing point. No.2', () {
+      final LyricSnippet targetSnippet = dataSetSnippet2.copyWith();
+      final characterPosition = 18;
+      final seekPosition = 7200;
+      final List<TimingPoint> expectedTimingPoints = [
+        TimingPoint(3, 400),
+        TimingPoint(2, 300),
+        TimingPoint(7, 700),
+        TimingPoint(0, 100),
+        TimingPoint(5, 600),
+        TimingPoint(1, 100),
+        TimingPoint(1, 100),
+      ];
+
+      timingService.addTimingPoint(targetSnippet, characterPosition, seekPosition);
+
+      expect(targetSnippet.timingPoints, expectedTimingPoints);
+    });
+
+    test('Test to add a timing point twice. No.1', () {
+      final LyricSnippet targetSnippet = dataSetSnippet2.copyWith();
+      final characterPosition = 5;
+      final seekPosition = 5450;
+      final List<TimingPoint> expectedTimingPoints = [
+        TimingPoint(3, 400),
+        TimingPoint(2, 50),
+        TimingPoint(0, 250),
+        TimingPoint(7, 700),
+        TimingPoint(0, 100),
+        TimingPoint(5, 600),
+        TimingPoint(2, 200),
+      ];
+
+      timingService.addTimingPoint(targetSnippet, characterPosition, seekPosition);
+
+      expect(targetSnippet.timingPoints, expectedTimingPoints);
+    });
+
+    test('Test to add a timing point twice. No.2', () {
+      final LyricSnippet targetSnippet = dataSetSnippet2.copyWith();
+      final characterPosition = 5;
+      final seekPosition = 5800;
+      final List<TimingPoint> expectedTimingPoints = [
+        TimingPoint(3, 400),
+        TimingPoint(2, 300),
+        TimingPoint(0, 100),
+        TimingPoint(7, 600),
+        TimingPoint(0, 100),
+        TimingPoint(5, 600),
+        TimingPoint(2, 200),
+      ];
+
+      timingService.addTimingPoint(targetSnippet, characterPosition, seekPosition);
+
+      expect(targetSnippet.timingPoints, expectedTimingPoints);
+    });
+
+    test('Test to throw TimingPointException when tring to add a timing point third time at the same char position. No.1', () {
+      final LyricSnippet targetSnippet = dataSetSnippet1.copyWith();
+      final characterPosition = 12;
+      final seekPosition = 5800;
+
+      expect(() => timingService.addTimingPoint(targetSnippet, characterPosition, seekPosition), throwsExceptionWithMessageContaining("Cannot add timing point more than 2"));
     });
   });
 }
