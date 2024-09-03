fd802e4606a0ddf29d4e24d547701942d047aa81
wh1te-star <sleeeping99@outlook.com>
2024-09-03 19:32:34 +0900

    Make the border adjustable.

--- lib/pane/adjustable_pane_border.dart
+++ lib/pane/adjustable_pane_border.dart
@@ -1,53 +1,65 @@
 import 'package:flutter/material.dart';
 
-class AdjustablePaneBorder extends StatelessWidget {
+class AdjustablePaneBorder extends StatefulWidget {
   final Widget textPane;
   final Widget timelinePane;
   final Widget videoPane;
 
   AdjustablePaneBorder({
+    Key? key,
     required this.videoPane,
     required this.textPane,
     required this.timelinePane,
-  });
+  }) : super(key: key);
+
+  @override
+  _AdjustablePaneBorderState createState() => _AdjustablePaneBorderState();
+}
+
+class _AdjustablePaneBorderState extends State<AdjustablePaneBorder> {
+  double videoPaneHeight = 400;
+  double textPaneHeight = 50;
 
   @override
   Widget build(BuildContext context) {
-    /*
-          return AdjustablePaneBorder(
-        child: Container(
-          width: horizontalBorderWidth,
-          color: Colors.grey,
-        ),
-        onHorizontalDragUpdate: (details) {
-          setState(() {
-            if (details.delta.dx > 0) {
-              LeftUpperPaneWidth += details.delta.dx;
-            } else {
-              if (LeftUpperPaneWidth > videoPaneWidthlimit) {
-                LeftUpperPaneWidth += details.delta.dx;
-              } else {
-                LeftUpperPaneWidth = videoPaneWidthlimit;
-              }
-            }
-          });
-        },
-        },
-        */
-    return Column(
-  children: [
-    Expanded(
-        child: videoPane,
-    ),
-    Expanded(
-        child: textPane,
-    ),
-    Expanded(
+    Widget videoTextPaneBorder = GestureDetector(
       child: Container(
-        child: timelinePane,
+        height: 10,
+        color: Colors.grey,
       ),
-    ),
-  ],
-);;
+      onVerticalDragUpdate: (details) {
+        videoPaneHeight += details.delta.dy;
+        setState(() {});
+      },
+    );
+    Widget textTimelinePaneBorder = GestureDetector(
+      child: Container(
+        height: 10,
+        color: Colors.grey,
+      ),
+      onVerticalDragUpdate: (details) {
+        textPaneHeight += details.delta.dy;
+        setState(() {});
+      },
+    );
+    return Column(
+      children: [
+        Container(
+          height: videoPaneHeight,
+          child: widget.videoPane,
+        ),
+        videoTextPaneBorder,
+        Container(
+          height: textPaneHeight,
+          child: widget.textPane,
+        ),
+        textTimelinePaneBorder,
+        Expanded(
+          child: Container(
+            child: widget.timelinePane,
+          ),
+        ),
+      ],
+    );
   }
 }
