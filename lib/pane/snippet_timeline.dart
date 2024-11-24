0bb3214fc0e2a0f7779d45e61e70c5ee1889a56a
wh1te-star <sleeeping99@outlook.com>
2024-11-24 13:49:07 +0900

    Show the timing point indicators.

--- lib/pane/snippet_timeline.dart
+++ lib/pane/snippet_timeline.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/widgets.dart';
 import 'package:lyric_editor/painter/rectangle_painter.dart';
+import 'package:lyric_editor/painter/triangle_painter.dart';
 import 'package:lyric_editor/service/timing_service.dart';
 import 'package:lyric_editor/utility/id_generator.dart';
 import 'package:lyric_editor/utility/lyric_snippet.dart';
@@ -46,17 +47,40 @@ class _SnippetTimelineState extends ConsumerState<SnippetTimeline> {
     this.intervalDuration,
   );
 
+  double length2Duration(double length) {
+    return length * intervalDuration / intervalLength;
+  }
+
+  double duration2Length(int duration) {
+    return duration.toDouble() * intervalLength / intervalDuration;
+  }
+
   @override
   Widget build(BuildContext context) {
     final TimingService timingService = ref.read(timingMasterProvider);
 
     List<Widget> snippetItemWidgets = [];
+    List<Widget> timingPointIndicatorWidgets = [];
     for (LyricSnippet snippet in snippets) {
-      Size itemSize = Size(
-        (snippet.endTimestamp - snippet.startTimestamp) * intervalLength / intervalDuration,
-        30.0,
-      );
-      Widget snippetItem = CustomPaint(
+      snippetItemWidgets.add(getSnippetItemWidget(snippet));
+      if (timingPointIndicatorWidgets.isEmpty) {
+        timingPointIndicatorWidgets += getTimingPointIndicatorWidgets(snippet);
+      }
+    }
+    return Stack(
+      children: snippetItemWidgets + timingPointIndicatorWidgets,
+    );
+  }
+
+  Widget getSnippetItemWidget(LyricSnippet snippet) {
+    Size itemSize = Size(
+      duration2Length(snippet.endTimestamp - snippet.startTimestamp),
+      30.0,
+    );
+    Widget snippetItem = Positioned(
+      left: duration2Length(snippet.startTimestamp),
+      top: 5.0,
+      child: CustomPaint(
         size: itemSize,
         painter: RectanglePainter(
           sentence: snippet.sentence,
@@ -64,16 +88,34 @@ class _SnippetTimelineState extends ConsumerState<SnippetTimeline> {
           isSelected: false,
           borderLineWidth: 2.0,
         ),
-      );
-      snippetItemWidgets.add(
-        Positioned(
-          left: snippet.startTimestamp * intervalLength / intervalDuration,
-          child: snippetItem,
+      ),
+    );
+    return snippetItem;
+  }
+
+  List<Widget> getTimingPointIndicatorWidgets(LyricSnippet snippet) {
+    Size itemSize = Size(
+      10.0,
+      10.0,
+    );
+
+    List<Widget> indicatorWidgets = [];
+    for (int index = 0; index < snippet.timingPoints.length; index++) {
+      TimingPoint timingPoint = snippet.timingPoints[index];
+      Widget indicator = Positioned(
+        left: duration2Length(snippet.startTimestamp + timingPoint.seekPosition),
+        child: CustomPaint(
+          size: itemSize,
+          painter: TrianglePainter(
+            x: 0.0,
+            y: 5.0,
+            width: 5.0,
+            height: 5.0,
+          ),
         ),
       );
+      indicatorWidgets.add(indicator);
     }
-    return Stack(
-      children: snippetItemWidgets,
-    );
+    return indicatorWidgets;
   }
 }
