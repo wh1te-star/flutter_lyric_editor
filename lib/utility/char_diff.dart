1445bda134c908f475a52907e61fdab996a68545
wh1te-star <sleeeping99@outlook.com>
2025-01-30 20:01:00 +0900

    Add temporary code.

--- lib/utility/char_diff.dart
+++ lib/utility/char_diff.dart
@@ -3,23 +3,54 @@ class CharDiff {
   String afterStr;
   List<DiffSegment> segments = [];
   CharDiff(this.beforeStr, this.afterStr) {
-    segments = [];
+    segments = getDiffSegments(beforeStr, afterStr);
+  }
+
+  List<DiffSegment> getDiffSegments(String beforeStr, String afterStr) {
+    List<DiffSegment> segments = [];
+    int beforeIndex = 0, afterIndex = 0;
+    while (beforeIndex < beforeStr.length || afterIndex < afterStr.length) {
+      if (beforeIndex < beforeStr.length && afterIndex < afterStr.length && beforeStr[beforeIndex] == afterStr[afterIndex]) {
+        // Find the length of the unchanged segment
+        int start = beforeIndex;
+        while (beforeIndex < beforeStr.length && afterIndex < afterStr.length && beforeStr[beforeIndex] == afterStr[afterIndex]) {
+          beforeIndex++;
+          afterIndex++;
+        }
+        segments.add(DiffSegment(beforeStr.substring(start, beforeIndex), afterStr.substring(start, afterIndex)));
+      } else {
+        // Handle edits, deletions, and additions
+        int startBefore = beforeIndex;
+        int startAfter = afterIndex;
+        while (beforeIndex < beforeStr.length && (afterIndex >= afterStr.length || beforeStr[beforeIndex] != afterStr[afterIndex])) {
+          beforeIndex++;
+        }
+        while (afterIndex < afterStr.length && (beforeIndex >= beforeStr.length || beforeStr[beforeIndex] != afterStr[afterIndex])) {
+          afterIndex++;
+        }
+        segments.add(DiffSegment(
+          beforeStr.substring(startBefore, beforeIndex),
+          afterStr.substring(startAfter, afterIndex),
+        ));
+      }
+    }
+    return segments;
   }
 }
 
 class DiffSegment {
-  EditType type;
   String beforeStr;
   String afterStr;
-  DiffSegment(this.type, this.beforeStr, this.afterStr) {
-    assert(beforeStr == "");
-    assert(afterStr == "");
-  }
-}
+  DiffSegment(this.beforeStr, this.afterStr);
 
-enum EditType {
-  none,
-  add,
-  delete,
-  edit,
+  @override
+  String toString() {
+    return "$beforeStr -> $afterStr";
+  }
+
+  @override
+  bool operator ==(Object other) => identical(this, other) || other is DiffSegment && runtimeType == other.runtimeType && beforeStr == other.beforeStr && afterStr == other.afterStr;
+
+  @override
+  int get hashCode => beforeStr.hashCode ^ afterStr.hashCode;
 }
--- test/char_diff_test.dart
+++ test/char_diff_test.dart
@@ -5,12 +5,58 @@ void main() {
   group('diff function test', () {
     setUp(() {});
 
+    test('When not editting any substring.', () {
+      const String beforeStr = "abcdefgh";
+      const String afterStr = "abcdefgh";
+      final CharDiff diff = CharDiff(beforeStr, afterStr);
+
+      final List<DiffSegment> expected = [
+        DiffSegment("abcdefgh", "abcdefgh"),
+      ];
+
+      expect(diff.segments, equals(expected));
+    });
+
     test('When editting a part of the before string. (e -> xx)', () {
       const String beforeStr = "abcdefgh";
       const String afterStr = "abcdxxfgh";
       final CharDiff diff = CharDiff(beforeStr, afterStr);
 
-      expect(diff.segments, isEmpty);
+      final List<DiffSegment> expected = [
+        DiffSegment("abcd", "abcd"),
+        DiffSegment("e", "xx"),
+        DiffSegment("fgh", "fgh"),
+      ];
+
+      expect(diff.segments, equals(expected));
+    });
+
+    test('When deleting a part of the before string. (delete e)', () {
+      const String beforeStr = "abcdefgh";
+      const String afterStr  = "abcdfgh";
+      final CharDiff diff = CharDiff(beforeStr, afterStr);
+
+      final List<DiffSegment> expected = [
+        DiffSegment("abcd", "abcd"),
+        DiffSegment("e", ""),
+        DiffSegment("fgh", "fgh"),
+      ];
+
+      expect(diff.segments, equals(expected));
+    });
+
+    test('When adding a part of the before string. (add xx)', () {
+      const String beforeStr = "abcdefgh";
+      const String afterStr = "abcdexxfgh";
+      final CharDiff diff = CharDiff(beforeStr, afterStr);
+
+      final List<DiffSegment> expected = [
+        DiffSegment("abcde", "abcde"),
+        DiffSegment("", "xx"),
+        DiffSegment("fgh", "fgh"),
+      ];
+
+      expect(diff.segments, equals(expected));
     });
   });
 }
