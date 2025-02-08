0b94a86eba24e0be571a8e4c63e8c7909a2f7e6c
wh1te-star <sleeeping99@outlook.com>
2025-02-08 18:13:32 +0900

    Implement the timing point list.

--- lib/lyric_snippet/timing_point/timing_point_list.dart
+++ lib/lyric_snippet/timing_point/timing_point_list.dart
@@ -1,35 +1,71 @@
+import 'dart:ffi';
+
+import 'package:collection/collection.dart';
 import 'package:lyric_editor/lyric_snippet/timing_point/timing_point.dart';
 
 class TimingPointList {
+  static const charPositionDuplicationAllowed = 2;
+  static const seekPositionDuplicationAllowed = 1;
   final List<TimingPoint> list;
 
-  TimingPointList(this.list);
+  TimingPointList(this.list) {
+    assert(isCharPositionOrdered());
+    assert(isSeekPositionOrdered());
+    assert(isCharPositionDuplicationAllowed());
+    assert(isSeekPositionDuplicationAllowed());
+  }
 
-  /*
-  BacktrackCell copyWith({
-    List<BacktrackRoute>? routes,
+  bool isCharPositionOrdered() {
+    return list.map((TimingPoint timingPoint) {
+      return timingPoint.charPosition;
+    }).isSorted((int left, int right) => left.compareTo(right));
+  }
+
+  bool isSeekPositionOrdered() {
+    return list.map((TimingPoint timingPoint) {
+      return timingPoint.seekPosition;
+    }).isSorted((int left, int right) => left.compareTo(right));
+  }
+
+  bool isCharPositionDuplicationAllowed() {
+    return groupBy(
+      list,
+      (TimingPoint timingPoint) => timingPoint.charPosition,
+    ).values.every((List<TimingPoint> group) => group.length <= charPositionDuplicationAllowed);
+  }
+
+  bool isSeekPositionDuplicationAllowed() {
+    return groupBy(
+      list,
+      (TimingPoint timingPoint) => timingPoint.seekPosition,
+    ).values.every((List<TimingPoint> group) => group.length <= seekPositionDuplicationAllowed);
+  }
+
+  TimingPointList copyWith({
+    TimingPointList? timingPointList,
   }) {
-    return BacktrackCell(
-      routes ?? _routes.map((route) => route.copyWith()).toList(),
+    return TimingPointList(
+      timingPointList?.list.map((TimingPoint timingPoint) => timingPoint.copyWith()).toList() ?? list,
     );
   }
 
   @override
   String toString() {
-    return _routes.join("\n");
+    return list.join("\n");
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-    if (other is! BacktrackCell) return false;
-    if (_routes.length != other._routes.length) return false;
-    return _routes.asMap().entries.every((entry) {
-      return entry.value == other._routes[entry.key];
+    if (other is! TimingPointList) return false;
+    if (list.length != other.list.length) return false;
+    return list.asMap().entries.every((MapEntry<int, TimingPoint> entry) {
+      int index = entry.key;
+      TimingPoint timingPoint = entry.value;
+      return timingPoint == other.list[index];
     });
   }
 
   @override
-  int get hashCode => const ListEquality().hash(_routes);
-  */
+  int get hashCode => const ListEquality().hash(list);
 }
