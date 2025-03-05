157e601aa23c2fe87b403c0b733569e50988d420
wh1te-star <sleeeping99@outlook.com>
2025-03-05 20:52:22 +0900

    Add the segment selection cursor mover.

---
+++ lib/pane/text_pane/cursor/segment_selection_cursor_mover.dart
@@ -0,0 +1,115 @@
+import 'package:lyric_editor/lyric_snippet/id/lyric_snippet_id.dart';
+import 'package:lyric_editor/lyric_snippet/lyric_snippet/lyric_snippet.dart';
+import 'package:lyric_editor/lyric_snippet/lyric_snippet/lyric_snippet_map.dart';
+import 'package:lyric_editor/pane/text_pane/cursor/annotation_selection_cursor_mover.dart';
+import 'package:lyric_editor/pane/text_pane/cursor/segment_selection_cursor.dart';
+import 'package:lyric_editor/pane/text_pane/cursor/text_pane_cursor_mover.dart';
+import 'package:lyric_editor/position/insertion_position.dart';
+import 'package:lyric_editor/position/seek_position.dart';
+import 'package:lyric_editor/position/segment_range.dart';
+import 'package:lyric_editor/service/timing_service.dart';
+import 'package:lyric_editor/utility/cursor_blinker.dart';
+
+class SegmentSelectionCursorMover extends TextPaneCursorMover {
+  SegmentSelectionCursorMover({
+    required super.lyricSnippetMap,
+    required super.textPaneCursor,
+    required super.cursorBlinker,
+    required super.seekPosition,
+  }) {
+    assert(textPaneCursor is SegmentSelectionCursor, "Wrong type textPaneCursor is passed: SegmentSelectionCursor is expected but ${textPaneCursor.runtimeType} is passed.");
+    assert(isIDContained(), "The passed lyricSnippetID does not point to a lyric snippet in lyricSnippetMap.");
+  }
+
+  bool isIDContained() {
+    if (textPaneCursor.isEmpty) {
+      return true;
+    }
+    LyricSnippet? lyricSnippet = lyricSnippetMap[textPaneCursor.lyricSnippetID];
+    if (lyricSnippet == null) {
+      return false;
+    }
+    return true;
+  }
+
+  factory SegmentSelectionCursorMover.withDefaultCursor({
+    required LyricSnippetMap lyricSnippetMap,
+    required LyricSnippetID lyricSnippetID,
+    required CursorBlinker cursorBlinker,
+    required SeekPosition seekPosition,
+  }) {
+    final SegmentSelectionCursor tempCursor = SegmentSelectionCursor(
+      lyricSnippetID,
+      cursorBlinker,
+      SegmentRange.empty,
+    );
+    final SegmentSelectionCursorMover tempMover = SegmentSelectionCursorMover(
+      lyricSnippetMap: lyricSnippetMap,
+      textPaneCursor: tempCursor,
+      cursorBlinker: cursorBlinker,
+      seekPosition: seekPosition,
+    );
+    return tempMover.copyWith(segmentSelectionCursor: tempMover.defaultCursor(lyricSnippetID));
+  }
+
+  @override
+  SegmentSelectionCursor defaultCursor(LyricSnippetID lyricSnippetID) {
+    LyricSnippet lyricSnippet = lyricSnippetMap.getLyricSnippetByID(lyricSnippetID);
+    int segmentIndex = lyricSnippet.getSegmentIndexFromSeekPosition(seekPosition);
+    return SegmentSelectionCursor(textPaneCursor.lyricSnippetID, cursorBlinker, SegmentRange(segmentIndex, segmentIndex));
+  }
+
+  @override
+  TextPaneCursorMover moveUpCursor() {
+    return this;
+  }
+
+  @override
+  TextPaneCursorMover moveDownCursor() {
+    return this;
+  }
+
+  @override
+  TextPaneCursorMover moveLeftCursor() {
+    return this;
+  }
+
+  @override
+  TextPaneCursorMover moveRightCursor() {
+    return this;
+  }
+
+  SegmentSelectionCursorMover copyWith({
+    LyricSnippetMap? lyricSnippetMap,
+    SegmentSelectionCursor? segmentSelectionCursor,
+    CursorBlinker? cursorBlinker,
+    SeekPosition? seekPosition,
+  }) {
+    return SegmentSelectionCursorMover(
+      lyricSnippetMap: lyricSnippetMap ?? this.lyricSnippetMap,
+      textPaneCursor: segmentSelectionCursor ?? textPaneCursor,
+      cursorBlinker: cursorBlinker ?? this.cursorBlinker,
+      seekPosition: seekPosition ?? this.seekPosition,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'SegmentSelectionCursorMover($lyricSnippetMap, $textPaneCursor, $cursorBlinker, $seekPosition)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (runtimeType != other.runtimeType) return false;
+    final SegmentSelectionCursorMover otherSegmentSelectionCursorMover = other as SegmentSelectionCursorMover;
+    if (lyricSnippetMap != otherSegmentSelectionCursorMover.lyricSnippetMap) return false;
+    if (textPaneCursor != otherSegmentSelectionCursorMover.textPaneCursor) return false;
+    if (cursorBlinker != otherSegmentSelectionCursorMover.cursorBlinker) return false;
+    if (seekPosition != otherSegmentSelectionCursorMover.seekPosition) return false;
+    return true;
+  }
+
+  @override
+  int get hashCode => lyricSnippetMap.hashCode ^ textPaneCursor.hashCode ^ cursorBlinker.hashCode ^ seekPosition.hashCode;
+}
