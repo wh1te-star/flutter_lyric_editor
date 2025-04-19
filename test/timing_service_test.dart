93339ed5214928aad88ca18b061e0dd389f4e20b
wh1te-star <sleeeping99@outlook.com>
2025-04-19 13:43:29 +0900

    Fix the add and delete a timing point tests.

--- test/timing_service_test.dart
+++ test/timing_service_test.dart
@@ -1,18 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:lyric_editor/lyric_snippet/lyric_snippet/lyric_snippet.dart';
 import 'package:lyric_editor/lyric_snippet/sentence_segment/sentence_segment.dart';
 import 'package:lyric_editor/lyric_snippet/sentence_segment/sentence_segment_list.dart';
 import 'package:lyric_editor/lyric_snippet/timing.dart';
-import 'package:lyric_editor/lyric_snippet/timing_point/timing_point.dart';
 import 'package:lyric_editor/lyric_snippet/timing_point_exception.dart';
 import 'package:lyric_editor/position/insertion_position.dart';
 import 'package:lyric_editor/position/seek_position.dart';
 import 'package:lyric_editor/service/timing_service.dart';
-import 'timing_service_test.mocks.dart';
 
 void main() {
-  group('Timing point test.', () {
+  group('Tests to add and delete a timing point.', () {
     final Timing dataSet1 = Timing(
       startTimestamp: SeekPosition(2000),
       sentenceSegmentList: SentenceSegmentList([
@@ -82,7 +78,9 @@ void main() {
 
       expect(
         () => target.addTimingPoint(characterPosition, seekPosition),
-        throwsA(predicate((e) => e is TimingPointException && e.message == "The seek position is out of the valid range.")),
+        throwsA(predicate(
+          (e) => e is TimingPointException && e.message == "The seek position is out of the valid range.",
+        )),
       );
     });
 
@@ -205,7 +203,9 @@ void main() {
 
       expect(
         () => target.addTimingPoint(characterPosition, seekPosition),
-        throwsA(predicate((e) => e is TimingPointException && e.message == "A timing point cannot be inserted three times or more at the same char position.")),
+        throwsA(predicate(
+          (e) => e is TimingPointException && e.message == "A timing point cannot be inserted three times or more at the same char position.",
+        )),
       );
     });
 
@@ -248,7 +248,9 @@ void main() {
 
       expect(
         () => target.deleteTimingPoint(characterPosition, option),
-        throwsA(predicate((e) => e is TimingPointException && e.message == "There is not specified timing point.")),
+        throwsA(predicate(
+          (e) => e is TimingPointException && e.message == "There is not the specified timing point.",
+        )),
       );
     });
 
@@ -288,10 +290,15 @@ void main() {
 
     test('Test to try to delete a non-existent timing point.', () {
       Timing target = dataSet2.copyWith();
-      final InsertionPosition characterPosition = InsertionPosition(17);
+      final InsertionPosition characterPosition = InsertionPosition(18);
       const option = Option.latter;
 
-      expect(() => target.deleteTimingPoint(characterPosition, option), TimingPointException("There is not specified timing point."));
+      expect(
+        () => target.deleteTimingPoint(characterPosition, option),
+        throwsA(predicate(
+          (e) => e is TimingPointException && e.message == "There is not the specified timing point.",
+        )),
+      );
     });
 
     test('Test to a former timing point of a character position.', () {
@@ -328,292 +335,4 @@ void main() {
       expect(target.sentenceSegmentList, expected);
     });
   });
-
-  /*
-  group('Edit sentence test.', () {
-    final Timing dataSet1 = Timing(
-      startTimestamp: SeekPosition(2000),
-      sentenceSegmentList: SentenceSegmentList([
-        SentenceSegment("a", Duration(milliseconds: 200)),
-        SentenceSegment("bcd", Duration(milliseconds: 400)),
-        SentenceSegment("efgh", Duration(milliseconds: 500)),
-        SentenceSegment("ij", Duration(milliseconds: 300)),
-      ]),
-    );
-    final Timing dataSet2 = Timing(
-      startTimestamp: SeekPosition(5000),
-      sentenceSegmentList: SentenceSegmentList([
-        SentenceSegment("abc", Duration(milliseconds: 400)),
-        SentenceSegment("d", Duration(milliseconds: 300)),
-        SentenceSegment("", Duration(milliseconds: 100)),
-        SentenceSegment("efg", Duration(milliseconds: 500)),
-        SentenceSegment("h", Duration(milliseconds: 100)),
-        SentenceSegment("ij", Duration(milliseconds: 300)),
-        SentenceSegment("", Duration(milliseconds: 100)),
-        SentenceSegment("kl", Duration(milliseconds: 200)),
-      ]),
-    );
-
-    TestWidgetsFlutterBinding.ensureInitialized();
-
-    setUp(() {});
-
-    test('Test the index translation when deleting a word.', () {
-      const String oldSentence = "abcde";
-      const String newSentence = "abce";
-      final List<int> expectedIndexTranslation = [0, 1, 2, 3, 3, 4];
-
-      final List<int> resultIndexTranslation = timingService.getCharPositionTranslation(oldSentence, newSentence);
-
-      expect(resultIndexTranslation, expectedIndexTranslation);
-    });
-
-    test('Test the index translation when adding a word.', () {
-      const String oldSentence = "abcde";
-      const String newSentence = "abcdxxe";
-      final List<int> expectedIndexTranslation = [0, 1, 2, 3, 4, 7];
-
-      final List<int> resultIndexTranslation = timingService.getCharPositionTranslation(oldSentence, newSentence);
-
-      expect(resultIndexTranslation, expectedIndexTranslation);
-    });
-
-    test('Test the index translation when editting a word. No.1', () {
-      const String oldSentence = "abcde";
-      const String newSentence = "abcxxe";
-      final List<int> expectedIndexTranslation = [0, 1, 2, 3, 5, 6];
-
-      final List<int> resultIndexTranslation = timingService.getCharPositionTranslation(oldSentence, newSentence);
-
-      expect(resultIndexTranslation, expectedIndexTranslation);
-    });
-
-    test('Test the index translation when editting a word. No.2', () {
-      const String oldSentence = "abcde";
-      const String newSentence = "axyzwze";
-      final List<int> expectedIndexTranslation = [0, 1, -1, -1, 6, 7];
-
-      final List<int> resultIndexTranslation = timingService.getCharPositionTranslation(oldSentence, newSentence);
-
-      expect(resultIndexTranslation, expectedIndexTranslation);
-    });
-
-    test('Test to edit a substring from a sentence. No.1', () {
-      final LyricSnippet targetSnippet = dataSet1.copyWith();
-      const String newSentence = "abcxxxhij";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(1, 200),
-        TimingPoint(6, 900),
-        TimingPoint(2, 300),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to delete a substring from a sentence. No.1', () {
-      final LyricSnippet targetSnippet = dataSet1.copyWith();
-      const String newSentence = "abchij";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(1, 200),
-        TimingPoint(3, 900),
-        TimingPoint(2, 300),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to add a string to a sentence. No.1', () {
-      final LyricSnippet targetSnippet = dataSet1.copyWith();
-      const String newSentence = "abcdefgxxhij";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(1, 200),
-        TimingPoint(3, 400),
-        TimingPoint(6, 500),
-        TimingPoint(2, 300),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to edit a substring from a sentence. No.2 (Edit just a segment)', () {
-      final LyricSnippet targetSnippet = dataSet1.copyWith();
-      const String newSentence = "abcdxxxij";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(1, 200),
-        TimingPoint(3, 400),
-        TimingPoint(3, 500),
-        TimingPoint(2, 300),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to delete a substring from a sentence. No.2 (Delete just a segment)', () {
-      final LyricSnippet targetSnippet = dataSet1.copyWith();
-      const String newSentence = "abcdij";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(1, 200),
-        TimingPoint(3, 400),
-        TimingPoint(0, 500),
-        TimingPoint(2, 300),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to add a string to a sentence. No.2 (Add a string at the same position of a timing point)', () {
-      final LyricSnippet targetSnippet = dataSet1.copyWith();
-      const String newSentence = "abcdxxefghij";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(1, 200),
-        TimingPoint(3, 400),
-        TimingPoint(6, 500),
-        TimingPoint(2, 300),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to edit a substring from a sentence. No.3', () {
-      final LyricSnippet targetSnippet = dataSetSnippet2.copyWith();
-      const String newSentence = "abxxxghijkl";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(6, 1300),
-        TimingPoint(1, 100),
-        TimingPoint(2, 300),
-        TimingPoint(0, 100),
-        TimingPoint(2, 200),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to delete a substring from a sentence. No.3', () {
-      final LyricSnippet targetSnippet = dataSetSnippet2.copyWith();
-      const String newSentence = "abghijkl";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(3, 1300),
-        TimingPoint(1, 100),
-        TimingPoint(2, 300),
-        TimingPoint(0, 100),
-        TimingPoint(2, 200),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to add a string from a sentence. No.3', () {
-      final LyricSnippet targetSnippet = dataSetSnippet2.copyWith();
-      const String newSentence = "abcdxxxefghijkl";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(3, 400),
-        TimingPoint(1, 300),
-        TimingPoint(0, 100),
-        TimingPoint(6, 500),
-        TimingPoint(1, 100),
-        TimingPoint(2, 300),
-        TimingPoint(0, 100),
-        TimingPoint(2, 200),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to edit a substring from a sentence. No.4 (Edit just a segment)', () {
-      final LyricSnippet targetSnippet = dataSetSnippet2.copyWith();
-      const String newSentence = "abcdxxhijkl";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(3, 400),
-        TimingPoint(1, 300),
-        TimingPoint(0, 100),
-        TimingPoint(2, 500),
-        TimingPoint(1, 100),
-        TimingPoint(2, 300),
-        TimingPoint(0, 100),
-        TimingPoint(2, 200),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to delete a substring from a sentence. No.4 (Delete just a segment)', () {
-      final LyricSnippet targetSnippet = dataSetSnippet2.copyWith();
-      const String newSentence = "abcdhijkl";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(3, 400),
-        TimingPoint(1, 300),
-        TimingPoint(0, 600),
-        TimingPoint(1, 100),
-        TimingPoint(2, 300),
-        TimingPoint(0, 100),
-        TimingPoint(2, 200),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test to delete a substring from a sentence. No.5 (Delete just some segment)', () {
-      final LyricSnippet targetSnippet = dataSetSnippet2.copyWith();
-      const String newSentence = "abcdijkl";
-      final List<TimingPoint> expectedSentenceSegments = [
-        TimingPoint(3, 400),
-        TimingPoint(1, 300),
-        TimingPoint(0, 700),
-        TimingPoint(2, 300),
-        TimingPoint(0, 100),
-        TimingPoint(2, 200),
-      ];
-
-      timingService.editSentence(targetSnippet, newSentence);
-
-      expect(targetSnippet.sentence, newSentence);
-      expect(targetSnippet._sentenceSegments, expectedSentenceSegments);
-    });
-
-    test('Test the seek position is not valid', () {
-      const String newSentence = "abcdefghijklmnopqrst";
-
-      /*
-      timingService.deleteTimingPoint(targetSnippet, characterPosition, option);
-
-      expect(targetSnippet.sentenceSegments, expectedSentenceSegments);
-
-      expect(() => timingService.addTimingPoint(targetSnippet, characterPosition, seekPosition), throwsExceptionWithMessageContaining("not valid"));
-      */
-    });
-  });
-*/
 }
