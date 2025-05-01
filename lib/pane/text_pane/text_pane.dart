cfda031d01e6bf86b32540acb8fa908eceeb2835
wh1te-star <sleeeping99@outlook.com>
2025-05-01 19:44:20 +0900

    Fix the imports.

--- lib/pane/text_pane/text_pane.dart
+++ lib/pane/text_pane/text_pane.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lyric_editor/lyric_snippet/annotation/annotation.dart';
-import 'package:lyric_editor/lyric_snippet/id/lyric_snippet_id.dart';
-import 'package:lyric_editor/lyric_snippet/lyric_snippet/lyric_snippet.dart';
-import 'package:lyric_editor/pane/text_pane/cursor/text_pane_cursor/sentence_selection_cursor.dart';
-import 'package:lyric_editor/pane/text_pane/cursor/text_pane_cursor/text_pane_cursor.dart';
 import 'package:lyric_editor/pane/text_pane/edit_column.dart';
 import 'package:lyric_editor/pane/text_pane/text_pane_provider.dart';
-import 'package:lyric_editor/position/insertion_position_info/insertion_position_info.dart';
-import 'package:lyric_editor/position/segment_range.dart';
-import 'package:lyric_editor/lyric_snippet/sentence_segment/sentence_segment.dart';
 import 'package:lyric_editor/service/music_player_service.dart';
 import 'package:lyric_editor/service/timing_service.dart';
-import 'package:lyric_editor/utility/sorted_list.dart';
-import 'package:lyric_editor/utility/utility_functions.dart';
-import 'package:tuple/tuple.dart';
 
 class TextPane extends ConsumerStatefulWidget {
   final FocusNode focusNode;
